6a19876178631c8aa2f679e3211684d329069502
kj3907 <rnjsrleo1@gmail.com>
2024-01-30 09:39:26 +0900

    bj11654 problem solving

---
+++ src/bj11654/Main.java
@@ -0,0 +1,12 @@
+package bj11654;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //아스키코드 문제
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        System.out.println((int)c);
+    }
+}
